5c6fac30578a136e00e79ed290da29f8c733b3e5
drosenbe <drosenbe@VDI0183.zit.bam.de>
2025-10-17 16:17:51 +0200

    adjusted syntax to use MLIAP interface and synchronized submit and swap script

--- submit_swap_potentials.sh
+++ submit_swap_potentials.sh
@@ -9,11 +9,16 @@
 
 #export OMP_NUM_THREADS=2
 eval "$(micromamba shell hook --shell bash)"
-micromamba activate mace_lammps
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/drosenbe/programs/lammps_mace_installed/lib:/home/drosenbe/programs/ACEsuit_lammps/libtorch/lib
+unset LD_LIBRARY_PATH
+micromamba activate mace_lammps_MLIAP
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/drosenbe/micromamba/envs/mace_lammps_MLIAP/lib:/home/drosenbe/programs/lammps_MLIAP_mace_installed/lib:/home/drosenbe/programs/lammps_mace_installed/lib:/home/drosenbe/programs/ACEsuit_lammps/libtorch/lib
+
+export OMPI_MCA_opal_cuda_support=true
+
 
 cd prod_run
-   for i in $(seq 1 2);
+   for i in $(seq 1 1);
    do	   
     if [ $i -eq 1 ]; then 
     cp ../energy_min_equib_first_classical/final_frame_classical.xyz .
@@ -21,10 +26,10 @@ cd prod_run
     #swap coordinates in MACE data File
     sed 's/XXX/'$i'/' ../swap_coordinates_lammpsio.py > tmp.py
     sed 's/MODEL/classical/' tmp.py >tmp2.py
-    sed 's/False/True/' tmp2.py > tmp3.py
+    sed -E 's/(write_new_data\s*\([^)]*MACE=)False/\1True/' tmp2.py > tmp3.py
     python tmp3.py
-    sed 's/XXX/'$i'/' ../lammps_settings/mixture_MACE_run.in > mixture_MACE_$i.in
-    mpirun -np 1 ~/programs/ACEsuit_lammps/lammps/build/lmp -k on g 1 -sf kk -in mixture_MACE_$i.in
+    sed 's/XXX/'$i'/' ../lammps_settings/mixture_MACE.in > mixture_MACE_$i.in
+    mpirun -np 1 /home/drosenbe/programs/lammps_MLIAP_mace_installed/bin/lmp  -k on g 1 -sf kk -pk kokkos newton on neigh half -in mixture_MACE_$i.in
     #extract final frame from MACE trajectory
     tac dump_EC_LiPF6_MACE_$i.lammpstrj | awk '/ITEM: TIMESTEP/ {exit} 1'| tac > final_frame_MACE.xyz
     #
@@ -32,8 +37,10 @@ cd prod_run
     sed 's/XXX/'$i'/' ../swap_coordinates_lammpsio.py > tmp.py
     sed 's/MODEL/MACE/' tmp.py > tmp2.py
     python tmp2.py
-    sed 's/XXX/'$i'/' ../lammps_settings/mixture_classical_run.in > mixture_classical_$i.in
-    mpirun -np 1 ~/programs/ACEsuit_lammps/lammps/build/lmp -k on g 1 -sf kk -in mixture_classical_$i.in
+    sed 's/XXX/'$i'/' ../lammps_settings/mixture_classical.in > mixture_classical_$i.in
+    mpirun -np 1 /home/drosenbe/programs/lammps_MLIAP_mace_installed/bin/lmp -k on g 1 -sf kk -in mixture_classical_$i.in
     tac dump_EC_LiPF6_classical_$i.lammpstrj | awk '/ITEM: TIMESTEP/ {exit} 1'| tac > final_frame_classical.xyz
     done
+rm tmp*
+cd ..   
 
